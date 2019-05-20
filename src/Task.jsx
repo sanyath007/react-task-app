870f73dc26690e6b136421482aec1c890e067e6c
sanyath007 <sanyath007@gmail.com>
2019-05-20 10:22:34 +0700

    Completed drag and drop appp with react-beautiful-dnd 1st version.

---
+++ src/Task.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+  display: flex;
+`;
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+  background-color: orange;
+  border-radius: 4px;
+`;
+
+class Task extends Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            <Handle {...provided.dragHandleProps} />
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
+
+export default Task;
